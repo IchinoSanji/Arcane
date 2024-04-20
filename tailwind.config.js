146efe8fe7a426aacbbd6f7b0b7e830c333f6f77
Lucas Eduardo <lucaseduardofeeh@gmail.com>
2024-04-20 01:28:12 -0300

    Landing Page Arcane 2 Tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+      'azul': '#3BC9F3'
+      },
+      height : {
+        '500' : '500px'
+      }
+    },
+  },
+  plugins: [],
+}
+
